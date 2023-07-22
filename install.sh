8ac4bd6d1a4abec1babc7d9cd316486dbaf4e9ab
Adam <shreyas.as.tech@protonmail.com>
2023-07-22 22:21:46 +0530

    Change screen timeout from 5 -> 4

--- install.sh
+++ install.sh
@@ -77,7 +77,7 @@ sudo sed -i "s/quiet/quiet video=1366x768/" /etc/default/grub
 tldr -u && sudo tldr -u # updating tldr pages for normal and root user
 sudo update-grub # grub tweaks (nothing major)
 echo "Section \"ServerFlags\"
-  Option \"BlankTime\" \"5\"
+  Option \"BlankTime\" \"4\"
 EndSection" | sudo tee -a /etc/X11/xorg.conf > /dev/null # to set screentimeout to 5 minutes
 
 # Done
