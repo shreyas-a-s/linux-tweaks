0f2681ded714f79593a94b7aa8ec732894e0e008
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-09-11 23:14:39 +0530

    Modify to read flatpak app names from txt file

--- scripts/flatpak.sh
+++ scripts/flatpak.sh
@@ -4,4 +4,23 @@ if [ "$DESKTOP_SESSION" == "gnome" ]; then
 	sudo apt install gnome-software-plugin-flatpak -y
 fi
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-flatpak install flathub com.discordapp.Discord com.github.tchx84.Flatseal com.obsproject.Studio com.slack.Slack com.spotify.Client de.haeckerfelix.Fragments net.cozic.joplin_desktop org.onlyoffice.desktopeditors -y
+
+# Specify the input file
+inputfile="./flatpak-applist.txt"
+
+# Check if the file exists
+if [ -e "$inputfile" ]; then
+    # Initialize a variable to store package names separated by spaces
+    packages=""
+
+    # Read each line from the file and accumulate the package names
+    while IFS= read -r package; do
+        # Add the package name to the list with a space
+        packages="$packages $package"
+    done < "$inputfile"
+
+    # Install all packages at once
+    flatpak install flathub $packages -y
+else
+    echo "File does not exist: $inputfile"
+fi
