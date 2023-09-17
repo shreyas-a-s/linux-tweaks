31b02a5477915cb4ae3e41d126354c207238e8a3
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-09-17 10:57:48 +0530

    Change variable name from fw -> choice

--- scripts/install-shell-customization.sh
+++ scripts/install-shell-customization.sh
@@ -3,8 +3,8 @@
 flag=true
 echo ""; echo "Some shell customisations are available for the shell..."
 while [ $flag == true ] ; do
-	read -r -p "...Do you use bash or fish? [1]bash [2]fish : " fw
-	if [ "$fw" == '1' ]; then
+	read -r -p "...Do you use bash or fish? [1]bash [2]fish : " choice
+	if [ "$choice" == '1' ]; then
 		sudo apt install bash-completion -y
 		cp dotfiles/bashextra ~/.config/.bashextra # my bash tweaks
 		echo "source /home/""$username""/.config/.bashextra" | tee -a ~/.bashrc > /dev/null
@@ -12,7 +12,7 @@ while [ $flag == true ] ; do
 		curl -sS https://starship.rs/install.sh | sh # installing starship
 		cp dotfiles/starship.toml ~/.config/ # startship configuration
 		flag=false
-	elif [ "$fw" == '2' ]; then
+	elif [ "$choice" == '2' ]; then
 		sudo apt install fish python-is-python3 -y
 		mkdir -p ~/.config/fish
 		cp dotfiles/config.fish ~/.config/fish/
