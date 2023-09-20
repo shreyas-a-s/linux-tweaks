9a3c68befc74dc02bf84c75fba681a5813abcfed
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-09-20 19:30:00 +0530

    Change directory before git clone

--- scripts/shell-customization.sh
+++ scripts/shell-customization.sh
@@ -5,7 +5,7 @@ function customiseBash {
     sudo apt install bash-completion -y
     cp dotfiles/bash_aliases ~/.bash_aliases # my bash tweaks
 	sudo cp dotfiles/fetch-master-6000 /usr/local/bin/fetch-master-6000 # Fetch command
-    (sudo apt update && sudo apt install git make -y && git clone https://gitlab.com/dwt1/shell-color-scripts.git && cd shell-color-scripts/ && sudo make install)
+    (sudo apt update && sudo apt install git make -y && cd .. && git clone https://gitlab.com/dwt1/shell-color-scripts.git && cd shell-color-scripts/ && sudo make install)
 	echo -e '#!/usr/bin/bash\nfetch-master-6000 --random --color random --length=10 --margin=4' | sudo tee /opt/shell-color-scripts/colorscripts/fetch-master-6000-exec > /dev/null
     sudo chmod +x /opt/shell-color-scripts/colorscripts/fetch-master-6000-exec
 }
