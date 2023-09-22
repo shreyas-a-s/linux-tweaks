d786001838abb9d4425a1fbc378f695aa729c331
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-09-22 11:17:29 +0530

    Update qemu.sh

--- scripts/qemu.sh
+++ scripts/qemu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-read -r -p "Continue to install qemu and virt-manager? (yes/no): " choice
-case "$choice" in 
+read -r -p "Continue to install qemu and virt-manager? (yes/no): " qemu_choice
+case "$qemu_choice" in 
     "yes" ) echo "Starting the installation.."; (cd .. && git clone https://github.com/shreyas-a-s/debian-qemu.git && cd debian-qemu/ && ./install.sh);;
     "no" ) exit 1;;
     * ) echo "Invalid Choice! Keep in mind this is CASE-SENSITIVE."; choiceOfQemu;;
