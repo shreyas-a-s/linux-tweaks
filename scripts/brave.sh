30e7ccbc0254edaf2f33d7c6cc7eae6804883528
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-09-22 11:08:35 +0530

    Update brave.sh

--- scripts/brave.sh
+++ scripts/brave.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt install curl
+sudo apt install curl -y
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 # To set third-party repositories to have least priority
