20fd565d7f04e82073969e802553311170814e76
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-09-22 13:30:56 +0530

    Correction of error of sed command syntax

--- scripts/shell-customization.sh
+++ scripts/shell-customization.sh
@@ -44,5 +44,5 @@ fi
 
 # Alias gedit to gnome-text-editor
 if [ "$DESKTOP_SESSION" == "gnome" ]; then
-    sed -i "a\alias gedit='gnome-text-editor'" ~/.bash_aliases
+    sed -i "$ a alias gedit='gnome-text-editor'" ~/.bash_aliases
 fi
