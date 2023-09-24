c548546d3a4b6c123f9e31113ec3f4800b21103b
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-09-24 16:33:53 +0530

    Add neofetch to /root/.bashrc

--- scripts/shell-customization.sh
+++ scripts/shell-customization.sh
@@ -8,6 +8,7 @@ function customiseBash {
     (sudo apt update && sudo apt install git make -y && cd .. && git clone https://gitlab.com/dwt1/shell-color-scripts.git && cd shell-color-scripts/ && sudo make install)
 	echo -e '#!/usr/bin/bash\nfetch-master-6000 --random --color random --length=10 --margin=4' | sudo tee /opt/shell-color-scripts/colorscripts/fetch-master-6000-exec > /dev/null
     sudo chmod +x /opt/shell-color-scripts/colorscripts/fetch-master-6000-exec
+	sudo sed -i '$ a\\n\#Neofetch\nif test -f "/usr/bin/neofetch"; then\n  neofetch\nfi' /root/.bashrc
 }
 
 # Function to customise fish shell
