c83bda4cec87a39eedb63afdcef71de47f556dc2
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-10-01 15:59:01 +0530

    Change lsd version for non-deb12-versions to 0.23.1 (fixed)

--- scripts/shell-customization.sh
+++ scripts/shell-customization.sh
@@ -36,8 +36,9 @@ function shellChoice {
 # lsd, the nexy-gen ls command
 function lsdInstall {
 	if [ "$debianversion" -lt 12 ]; then
-		wget -q -nv -O - https://api.github.com/repos/lsd-rs/lsd/releases/latest   | awk -F': ' '/browser_download_url/ && /\_amd64.deb/ \
-	                             {gsub(/"/, "", $(NF)); system("wget -qi -L " $(NF))}' && rm lsd-musl* && mv *.deb lsd.deb && sudo dpkg -i lsd.deb && rm lsd.deb
+		wget https://github.com/lsd-rs/lsd/releases/download/0.23.1/lsd_0.23.1_amd64.deb
+		sudo dpkg -i lsd_0.23.1_amd64.deb
+		rm lsd.deb
 	else
 		sudo apt-get -y install lsd
 	fi
