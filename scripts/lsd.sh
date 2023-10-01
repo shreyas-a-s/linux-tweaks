5632a3a8ac4bf9df6a24a0dd669f6913ca4fb08f
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-10-01 15:07:01 +0530

    Delete lsd.sh

--- scripts/lsd.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo ""; echo "Installing lsd, the next gen 'ls' command..."
-if [ "$debianversion" -lt 12 ]; then
-	wget -q -nv -O - https://api.github.com/repos/lsd-rs/lsd/releases/latest   | awk -F': ' '/browser_download_url/ && /\_amd64.deb/ \
-	                             {gsub(/"/, "", $(NF)); system("wget -qi -L " $(NF))}' && rm lsd-musl* && mv *.deb lsd.deb && sudo dpkg -i lsd.deb && rm lsd.deb
-else
-	sudo apt-get -y install lsd
-fi
