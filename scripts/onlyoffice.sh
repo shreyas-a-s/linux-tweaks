983061dbfef1f55e492cb0004ac35dfdca598004
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-10-24 03:38:17 +0530

    Create onlyoffice.sh

---
+++ scripts/onlyoffice.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+mkdir -p -m 700 ~/.gnupg
+gpg --no-default-keyring --keyring gnupg-ring:/tmp/onlyoffice.gpg --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys CB2DE8E5
+chmod 644 /tmp/onlyoffice.gpg
+sudo chown root:root /tmp/onlyoffice.gpg
+sudo mv /tmp/onlyoffice.gpg /usr/share/keyrings/onlyoffice.gpg
+echo 'deb [signed-by=/usr/share/keyrings/onlyoffice.gpg] https://download.onlyoffice.com/repo/debian squeeze main' | sudo tee -a /etc/apt/sources.list.d/onlyoffice.list
+# To set third-party repositories to have least priority
+echo -e 'Package: *\nPin: origin download.onlyoffice.com\nPin-Priority: 100' | sudo tee /etc/apt/preferences.d/onlyoffice.pref > /dev/null
+sudo apt-get update && sudo apt-get -y install onlyoffice-desktopeditors
