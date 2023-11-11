79ab0752b94e0dc012d46ebffd4df3d1ccbbe449
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-11-12 01:11:53 +0530

    Update config.fish

--- dotfiles/config.fish
+++ dotfiles/config.fish
@@ -1,175 +1,176 @@
-###############################################################################
-#################### --<<<-( Disable Fish Greeting )->>>-- ####################
-###############################################################################
-
+### DISABLE GREETING ###
 set -U fish_greeting
 
-
-###############################################################################
-################### --<<<-( Variables & Other stuff )->>>-- ###################
-###############################################################################
-
-# Variables
-set DATE $(date -I)
-
-# Colored Man Pages
-set MANPAGER "sh -c 'col -bx | batcat -l man -p'"
-export MANPAGER
-
-# DT's Shell Color Scripts
-if test -f /usr/local/bin/colorscript;
-    bash colorscript --random;
+### EXPORT ###
+setenv TERM "xterm-256color" # getting proper colors
+if test -f /usr/bin/micro
+  setenv EDITOR "micro"
+end
+if test -f /usr/bin/codium
+  setenv VISUAL "codium"
 end
 
-###############################################################################
-################## --<<<-( Colors for the ls command )->>>-- ##################
-###############################################################################
+#### SET MANPAGER ###
+setenv MANPAGER "sh -c 'col -bx | batcat -l man -p'"
 
-if test -f ~/.config/lscolors/lscolors.csh;
-    source ~/.config/lscolors/lscolors.csh;
+### PATH ###
+if test -d "$HOME/.bin"
+  then PATH="$HOME/.bin:$PATH"
 end
 
-###############################################################################
-####################### --<<<-( Autojump Config )->>>-- #######################
-###############################################################################
-
-if test -f /usr/share/autojump/autojump.fish;
-    . /usr/share/autojump/autojump.fish;
+if test -d "$HOME/.local/bin"
+  then PATH="$HOME/.local/bin:$PATH"
 end
 
-###############################################################################
-##################### --<<<-( User Defined Aliases )->>>-- ####################
-###############################################################################
+if test -d "$HOME/Applications"
+  then PATH="$HOME/Applications:$PATH"
+end
 
-# "Useless" cat -> "Useless" batcat
-alias cat='batcat --style=plain'
+if test -d "/var/lib/flatpak/exports/bin/"
+  then PATH="/var/lib/flatpak/exports/bin/:$PATH"
+end  
 
-# ls
-alias la='lsd -A --icon never'
-alias ls='lsd --icon never'
-alias ll='lsd -Al --icon never --date "+%-d/%-m %H:%M" --size short --blocks permission,user,size,date,name'
+### ALIASES ###
 
-# file/directory operations
+# Changing "ls" to "exa"
+if test -f "/usr/bin/exa"
+    alias ls='exa -a --color=always --group-directories-first'  # all files and dirs
+    alias ll='exa -al --color=always --group-directories-first' # my preferred listing
+    alias lt='exa -aT --color=always --group-directories-first' # tree listing
+else
+    alias ls='ls --color=auto'
+    alias la='ls -A --color=auto'
+    alias ll='ls -alh --color=auto'
+end
+
+# Colorize grep output (good for log files)
+alias grep='grep --color=auto'
+alias egrep='egrep --color=auto'
+alias fgrep='fgrep --color=auto'
+
+# Adding flags
+alias df='df -h'                          # human-readable sizes
+alias free='free -m'                      # show sizes in MB
+
+# Add verbose output to cp, mv & mkdir
 alias cp='cp -iv'
 alias mv='mv -iv'
-alias mkdir='mkdir -pv' # (-p) create directory and parents (-v) print a message for each directory created
-alias rm='trash -v'
+alias mkdir='mkdir -pv'
 
-# ShellCheck
+# Enable command-line trash
+if test -f "/usr/bin/trash"
+  alias rm='trash'
+end
+
+# Colorize cat command
+if test -f "/usr/bin/batcat"
+  alias cat='batcat --style=plain'
+end
+
+# Allows shellcheck to follow any file the script may source
 alias shellcheck='shellcheck -x'
 
-# apt package manager
-# alias aupdate='sudo apt update'
-# alias aupgrade='sudo apt upgrade -y && am -u'
-# alias asearch='apt search'
-# alias ashow='apt show'
-# alias ainstall='sudo apt install'
-# alias apurge='sudo apt purge'
-# alias aautoremove='sudo apt autoremove -y'
-# alias adepends='apt depends'
-# alias ardepends='apt rdepends'
-# alias alist='apt list'
-
-# text editor
-# alias edit='micro'
-# alias sedit='sudo micro'
-
-# history
-alias history='history -R | cat -n'
-
-# git
-# alias gstatus='git status'
-# alias gadd='git add'
-# alias gaddu='git add -u'
-# alias gcommit='git commit -m'
-# alias gpush='git push'
-# alias glog='git log'
-# alias gfetch='git fetch'
-# alias gmerge='git merge'
-# alias gfetchmerge='git fetch && git merge'
-
-# Kill a process after searching using dmenu
-alias dkill='ps aux | awk \'NR!=1 {print "Process: "$11}\'  | dmenu -i -p "Search for the process to kill:" -sb "#1D7C3A" -sf "#FFFFFF" | awk \'{print $2}\' | xargs pkill -f'
-
-# Some extra aliases
-alias grep='grep --color=auto'
-alias shellcheck='shellcheck -x'
+# Force start btop even if no UTF-8 locale was detected
 alias btop='btop --utf-force'
 
-###############################################################################
-#################### --<<<-( User Defined Functions )->>>-- ###################
-###############################################################################
+# Making history show numbers like other shells
+alias history='history -R | cat -n'
+
+### RANDOM COLOR SCRIPT ###
+# Get this script from my Github: github.com/shreyas-a-s/shell-color-scripts
+if test -f /usr/local/bin/colorscript
+  bash colorscript random
+end
+
+### AUTOJUMP ###
+if test -f /usr/share/autojump/autojump.fish
+  . /usr/share/autojump/autojump.fish
+else
+  echo "can't found the autojump script"
+end
+
+# Set colors for ls command
+if test -f "$HOME/.config/lscolors/lscolors.csh"
+  source ~/.config/lscolors/lscolors.csh
+end
+
+### FUNCTIONS ###
 
 # Extract all types of archive file formats
 function extract
-    set archiveextension (echo "$argv" | awk -F . {'print $NF'})
-    set archiveextensionprefix (echo "$argv" | awk -F . {'print $(NF-1)'})
-    switch $archiveextension
-		case bz2
-			if [ "$archiveextensionprefix" = "tar" ]
-				tar xjf "$argv"
-			else
-				bunzip2 "$argv"
-			end
-		case gz
-			if [ "$archiveextensionprefix" = "tar" ]
-				tar xzf "$argv"
-			else
-				gunzip "$argv"
-			end
-		case rar
-			unrar x "$argv"
-		case tar
-			tar xf "$argv"
-		case tbz2
-			tar xjf "$argv"
-		case tgz
-			tar xzf "$argv"
-		case zip
-			unzip "$argv"
-		case Z
-			uncompress "$argv"
-		case 7z
-			7z x "$argv"
-		case deb
-			ar x "$argv"
-		case xz
-			tar xf "$argv"
-		case zst
-			unzstd "$argv"
-		case '*'
-			echo "sorry, brother. I have no idea what you are trying to extract."
-    end
+  set archiveextension (echo "$argv" | awk -F . {'print $NF'})
+  set archiveextensionprefix (echo "$argv" | awk -F . {'print $(NF-1)'})
+  switch $archiveextension
+	case bz2
+	  if [ "$archiveextensionprefix" = "tar" ]
+		tar xjf "$argv"
+	  else
+		bunzip2 "$argv"
+	  end
+	case gz
+	  if [ "$archiveextensionprefix" = "tar" ]
+		tar xzf "$argv"
+	  else
+		gunzip "$argv"
+	  end
+	case rar
+		unrar x "$argv"
+	case tar
+		tar xf "$argv"
+	case tbz2
+		tar xjf "$argv"
+	case tgz
+		tar xzf "$argv"
+	case zip
+		unzip "$argv"
+	case Z
+		uncompress "$argv"
+	case 7z
+		7z x "$argv"
+	case deb
+		ar x "$argv"
+	case xz
+		tar xf "$argv"
+	case zst
+		unzstd "$argv"
+	case '*'
+		echo "sorry, brother. I have no idea what you are trying to extract."
+  end
 end
 
-# Make a directory (and parents if necessary) & move to that directory
+# Create and go to the directory
 function mkdircd
-    mkdir -pv "$argv"
-    cd "$argv"
+  mkdir -pv "$argv"
+  cd "$argv"
 end
 
 # Replacement for Bash 'sudo !!' command & replacing 'apt' with 'nala'
 function sudo
-    if test "$argv" = !!
-        echo sudo $history[1]
-        eval command sudo $history[1]
-    else if test "$argv[1]" = apt -a -f /usr/bin/nala
-        set argv[1] nala && command sudo $argv
-    else
-        command sudo $argv
-    end
-end
-
-# Function to use ix.io (the command-line pastebin)
-function ix
-    curl -F "f:1=@$argv[1]" ix.io
+  if test "$argv" = !!
+	echo sudo $history[1]
+	eval command sudo $history[1]
+  else if test "$argv[1]" = apt -a -f /usr/bin/nala
+	set argv[1] nala && command sudo $argv
+  else
+	command sudo $argv
+  end
 end
 
 # My Ping ;)
 function ping
-	if test -z "$argv[1]"
-		command ping -c 1 example.org
-	else
-		command ping $argv
-	end
+  if test -z "$argv[1]"
+	command ping -c 1 example.org
+  else
+	command ping $argv
+  end
+end
+
+# Function to use ix.io (the command-line pastebin)
+function ix
+  curl -F "f:1=@$argv[1]" ix.io
+end
+
+### SETTING THE STARSHIP PROMPT ###
+if [ -f /usr/local/bin/starship ]
+  starship init fish | source
 end
