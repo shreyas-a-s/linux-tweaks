69d090ca303285d98675802555635b59d038c843
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-11-12 13:41:06 +0530

    Remove Bash Line Editor

--- scripts/shell-customization.sh
+++ scripts/shell-customization.sh
@@ -35,11 +35,7 @@ function customiseBash {
 	# Copy necessary files
 	cp ../dotfiles/.bashrc ~/
 	cp ../dotfiles/lscolors.sh ~/.config/lscolors/
-	
-	# Bash Line Editor by @akinomyoga on github
-	git clone --recursive --depth 1 --shallow-submodules https://github.com/akinomyoga/ble.sh.git
-	make -C ble.sh install PREFIX=~/.local
-	echo -e "\n\n#Bash Line Editor by @akinomyoga on github\nsource ~/.local/share/blesh/ble.sh" >> ~/.bashrc
+
 }
 
 # Function to customise fish shell
