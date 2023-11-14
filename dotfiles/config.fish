3ce3a4bcbccbc7c151b37c85790f1544d235ebfe
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-11-15 01:06:09 +0530

    Added a function for history substitution using index number of command

--- dotfiles/config.fish
+++ dotfiles/config.fish
@@ -105,6 +105,16 @@ end
 
 ### FUNCTIONS ###
 
+# Replacement for !number history substitution feature in bash and zsh
+# Usage: h <index number of command>
+# NOTE: Index number is the number preceding the command in the output of 'history'
+function h
+  set prgmcmd (history -R | command cat -n | awk "NR==$argv{print $0}" | awk '{print $2}')
+  set prgmargs (history -R | command cat -n | awk "NR==$argv{print $0}" | awk -F "$prgmcmd" '{print $2}')
+  echo "$prgmcmd $prgmargs"
+  eval "$prgmcmd $prgmargs"
+end
+
 # Git functions
 function gcom
   git add .
