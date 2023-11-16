37b4611de3eaa0feec5a150a17cff826823e1074
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-11-16 19:02:59 +0530

    Making output of neovim-appimage-updater.sh better

--- scripts/neovim-appimage-updater.sh
+++ scripts/neovim-appimage-updater.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-if whereis nvim | awk '{print $2}' | grep nvim > /dev/null; then
+printf "\nLooking for Neovim AppImage updates..."
+
+if whereis nvim | awk '{print $2}' | grep -q nvim; then
   
   currentnvimversion=$(nvim -v | awk -F 'NVIM v' 'NR==1{print $2}')                                               
   latestnvimversion=$(curl -s https://github.com/neovim/neovim/releases/tag/stable | grep 'NVIM v' | awk -F 'NVIM v' 'NR==1{print $2}')
@@ -12,18 +14,25 @@ if whereis nvim | awk '{print $2}' | grep nvim > /dev/null; then
   latestpatchversion=$(echo $latestnvimversion | awk -F . '{print $3}')
   
   if [ $currentminorversion -lt $latestminorversion ] || ([ $currentminorversion -eq $latestminorversion ] && [ $currentpatchversion -lt $latestpatchversion ]); then
+    
+    printf "\n\n Found a new version on Github. Installing it...\n"
+    sleep 1
 
     wget https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
     chmod +x nvim.appimage
     sudo mv nvim.appimage /usr/local/bin/nvim
 
   else
-
-    printf "\nNothing to do. Neovim is up-to-date with version: $latestnvimversion"
+    
+    printf "\n\n"
+    echo "Nothing to do. Neovim is up-to-date with version: $latestnvimversion"
 
   fi
 
 else
+
+  printf "/n/nNeovim is not installed. Installing now...\n"
+  sleep 1
 
   wget https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
   chmod +x nvim.appimage
