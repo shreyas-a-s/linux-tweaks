e5e11ec31f96f24655f157e3ac8ea43aeac46039
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-11-21 23:36:31 +0530

    Remove extract functions from bash, zsh and fish

--- dotfiles/config.fish
+++ dotfiles/config.fish
@@ -176,48 +176,6 @@ function ix
   curl -F "f:1=@$argv[1]" ix.io
 end
 
-# Function to extract common file formats
-function extract
-  set archiveextension (echo "$argv" | awk -F . {'print $NF'})
-  set archiveextensionprefix (echo "$argv" | awk -F . {'print $(NF-1)'})
-  switch $archiveextension
-	case bz2
-	  if [ "$archiveextensionprefix" = "tar" ]
-		tar xjf "$argv"
-	  else
-		bunzip2 "$argv"
-	  end
-	case gz
-	  if [ "$archiveextensionprefix" = "tar" ]
-		tar xzf "$argv"
-	  else
-		gunzip "$argv"
-	  end
-	case rar
-		unrar x "$argv"
-	case tar
-		tar xf "$argv"
-	case tbz2
-		tar xjf "$argv"
-	case tgz
-		tar xzf "$argv"
-	case zip
-		unzip "$argv"
-	case Z
-		uncompress "$argv"
-	case 7z
-		7z x "$argv"
-	case deb
-		ar x "$argv"
-	case xz
-		tar xf "$argv"
-	case zst
-		unzstd "$argv"
-	case '*'
-		echo "sorry, brother. I have no idea what you are trying to extract."
-  end
-end
-
 ### SETTING THE STARSHIP PROMPT ###
 if which starship > /dev/null
   starship init fish | source
