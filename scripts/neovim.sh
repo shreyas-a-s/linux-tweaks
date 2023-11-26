bbed8491ca685dd4f5729860b454971ef0975d0c
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-11-26 21:59:14 +0530

    Rename neovim-appimage-updater.sh to neovim-appimage-updater

--- scripts/neovim.sh
+++ scripts/neovim.sh
@@ -6,6 +6,6 @@ SCRIPT_DIR=$(dirname -- "$( readlink -f -- "$0"; )") && cd "$SCRIPT_DIR" || exit
 if [ "$(apt-cache show neovim | grep Version | awk -F '.' '{print $2}')" -ge 9 ]; then
   sudo apt-get install -y neovim
 else
-  ./neovim-appimage-updater.sh
-  sudo cp neovim-appimage-updater.sh /usr/local/bin/neovim-appimage-updater
+  ./neovim-appimage-updater
+  sudo cp neovim-appimage-updater /usr/local/bin/
 fi
