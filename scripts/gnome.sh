e3bcba5023150276d69a775568fdc7e2c89b5648
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-11-29 22:03:37 +0530

    Change gsconnect installing from apt to native implementation

--- scripts/gnome.sh
+++ scripts/gnome.sh
@@ -9,7 +9,7 @@ SCRIPT_DIR=$(dirname -- "$( readlink -f -- "$0"; )") && cd "$SCRIPT_DIR" || exit
 # Begin
 sudo apt-get update
 sudo apt-get -y purge firefox-esr yelp gnome-terminal totem gnome-software gnome-characters gnome-contacts gnome-font-viewer gnome-logs byobu epiphany-browser kdeconnect keepassxc
-sudo apt-get -y install gnome-console gnome-tweaks gnome-text-editor gnome-shell-extension-gsconnect secrets fonts-cantarell nautilus baobab gnome-calculator
+sudo apt-get -y install gnome-console gnome-tweaks gnome-text-editor secrets fonts-cantarell nautilus baobab gnome-calculator
 sudo apt-get -y autoremove
 
 # Symlink gedit to gnome-text-editor
@@ -17,6 +17,9 @@ sudo ln -s /usr/bin/gnome-text-editor /usr/bin/gedit
 
 # Install Rounded Window Corner
 busctl --user call org.gnome.Shell.Extensions /org/gnome/Shell/Extensions org.gnome.Shell.Extensions InstallRemoteExtension s rounded-window-corners@yilozt
+
+# Install GSConnect
+busctl --user call org.gnome.Shell.Extensions /org/gnome/Shell/Extensions org.gnome.Shell.Extensions InstallRemoteExtension s gsconnect@andyholmes.github.io
 
 # Uninstall qbittorrent if present
 which qbittorrent > /dev/null && sudo apt purge -y qbittorrent
