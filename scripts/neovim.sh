b4f6b1be5f2307dc3ab7551709a1621519f66a34
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-12-18 13:36:59 +0530

    Add error avoidance to neovim.sh

--- scripts/neovim.sh
+++ scripts/neovim.sh
@@ -4,15 +4,16 @@
 SCRIPT_DIR=$(dirname -- "$( readlink -f -- "$0"; )") && cd "$SCRIPT_DIR" || exit
 
 # Install gcc which is a dependancy of a neovim plugin
-sudo apt-get update && sudo apt-get install -y gcc
+if command -v apt-get > /dev/null; then
+  sudo apt-get update
+  sudo apt-get install -y gcc
+fi
 
 # Install neovim
 if [ "$(apt-cache show neovim | grep Version | awk -F '.' '{print $2}')" -ge 9 ]; then
   sudo apt-get install -y neovim
 else
-  if ! command -v snap > /dev/null; then
-    sudo apt-get install -y snapd
-  fi
+  ./snap.sh
   sudo snap install nvim 2>/dev/null || sudo snap install nvim --classic
   sudo ln -s /snap/bin/nvim /usr/local/bin/
 fi
