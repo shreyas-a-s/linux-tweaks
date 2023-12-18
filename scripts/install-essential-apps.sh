d11a682cfe09ec49d2a529e55572d183d61e15d8
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-12-18 15:02:54 +0530

    Add 'change directory' command to install-essential-apps.sh

--- scripts/install-essential-apps.sh
+++ scripts/install-essential-apps.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Change directory
+SCRIPT_DIR=$(dirname -- "$( readlink -f -- "$0"; )") && cd "$SCRIPT_DIR" || exit
+
 if command -v apt-get > /dev/null; then
   xargs -a "essential-apps.txt" sudo apt-get install -y python-is-python3
 fi
