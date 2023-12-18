dd9ec7c0f7d05c20735deb0bb8d3dc563abbb90c
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-12-18 13:39:58 +0530

    Add error avoidance to snap.sh

--- scripts/snap.sh
+++ scripts/snap.sh
@@ -1,7 +1,15 @@
 #!/bin/sh
 
+# Check if snap is already installed
+if command -v snap > /dev/null; then
+  echo "Error: snap is already installed at $(command -v snap)" 2>&1
+  exit 1
+fi
+
 # Install snapd
-sudo apt-get update && sudo apt-get install -y snapd
+if command -v apt-get > /dev/null; then
+  sudo apt-get update && sudo apt-get install -y snapd
+fi
 
 # Install snap apps that I use
 sudo snap install joplin-desktop
