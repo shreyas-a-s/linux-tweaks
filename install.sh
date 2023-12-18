41a409700be968f32437914ab2dc9896acfec01a
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-12-18 14:53:49 +0530

    Rename gnome.sh to setup-gnome.sh

--- install.sh
+++ install.sh
@@ -21,7 +21,7 @@ SCRIPT_DIR=$(dirname -- "$( readlink -f -- "$0"; )") && cd "$SCRIPT_DIR" || exit
 ./scripts/install-brave.sh # brave-browser
 ./scripts/install-essential-apps.sh # As the name suggests
 ./scripts/install-github-desktop.sh # github-desktop for linux
-./scripts/gnome.sh # GNOME Desktop Environment Customisations
+./scripts/setup-gnome.sh # GNOME Desktop Environment Customisations
 ./scripts/nala.sh # apt, but colorful
 ./scripts/onlyoffice.sh # office suite
 ./scripts/setup-antix.sh # Antix Linux Customisations
