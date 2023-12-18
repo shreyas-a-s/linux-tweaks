1c5cc14bd15cc6e2af57fa46dcc70398268b5729
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-12-18 13:33:00 +0530

    Add error avoidance to lsd.sh

--- scripts/lsd.sh
+++ scripts/lsd.sh
@@ -2,7 +2,7 @@
 
 if apt-cache show lsd > /dev/null; then
   sudo apt-get install -y lsd
-else
+elif command -v dpkg > /dev/null; then
   wget https://github.com/lsd-rs/lsd/releases/download/0.23.1/lsd_0.23.1_amd64.deb
   sudo dpkg -i lsd_0.23.1_amd64.deb
   rm lsd_0.23.1_amd64.deb
