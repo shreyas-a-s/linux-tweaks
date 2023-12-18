72916ef3ce97a77780abc8ac73aa129aab9ae1f5
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-12-18 15:03:26 +0530

    Add 'change directory' command to install-terminal-apps.sh

--- scripts/install-terminal-apps.sh
+++ scripts/install-terminal-apps.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+# Change directory
+SCRIPT_DIR=$(dirname -- "$( readlink -f -- "$0"; )") && cd "$SCRIPT_DIR" || exit
+
 if command -v apt-get > /dev/null; then
   xargs -a "terminal-apps.txt" sudo apt-get install -y
 fi
