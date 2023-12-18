489e0fcadb2c0d96258bfcbaf02b0dd95bbe7c64
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-12-18 12:09:14 +0530

    Add command to execute newly added scripts to install.sh

--- install.sh
+++ install.sh
@@ -20,12 +20,14 @@ SCRIPT_DIR=$(dirname -- "$( readlink -f -- "$0"; )") && cd "$SCRIPT_DIR" || exit
 sudo apt-get -y install ufw man git gparted vlc shellcheck curl wget python-is-python3 obs-studio kdeconnect keepassxc qbittorrent
 
 # My custom scripts
+./scripts/update-system.sh # Updating installed programs
 ./scripts/brave.sh # brave-browser
 ./scripts/github-desktop.sh # github-desktop for linux
-./scripts/gnome.sh # gnome-specific-customisations
+./scripts/gnome.sh # GNOME Desktop Environment Customisations
 ./scripts/librewolf.sh # firefox fork that is truely the best (IMO)
 ./scripts/nala.sh # apt, but colorful
 ./scripts/onlyoffice.sh # office suite
+./scripts/setup-antix.sh # Antix Linux Customisations
 ./scripts/snap.sh # snap package manager
 ./scripts/vscodium.sh # open source vscode
 ./scripts/shell-customization.sh # bash/fish/zsh customizations
