a4543e36262d888b8e66b237d3ef57ec175b3a8e
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-12-19 18:03:47 +0530

    Change indentation

--- scripts/install-command-not-found.sh
+++ scripts/install-command-not-found.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 if command -v apt-get > /dev/null; then # Install for debian-based distros
+
+  # Install the app
   sudo apt-get install -y command-not-found
 
   # Update database of command-not-found
--- scripts/install-essential-apps.sh
+++ scripts/install-essential-apps.sh
@@ -4,6 +4,7 @@
 SCRIPT_DIR=$(dirname -- "$( readlink -f -- "$0"; )") && cd "$SCRIPT_DIR" || exit
 
 if command -v apt-get > /dev/null; then
+
   xargs -a "essential-apps.txt" sudo apt-get install -y python-is-python3
 fi
 
