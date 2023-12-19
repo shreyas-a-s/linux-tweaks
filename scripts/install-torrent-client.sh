f839b383f854eb5a57dd16a26931e52d78383b9c
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-12-20 01:24:05 +0530

    Fix appearance of 'E: No packages found' when running apt-cache show command

--- scripts/install-torrent-client.sh
+++ scripts/install-torrent-client.sh
@@ -3,14 +3,14 @@
 # Change directory
 SCRIPT_DIR=$(dirname -- "$( readlink -f -- "$0"; )") && cd "$SCRIPT_DIR" || exit
 
-if [ "$XDG_CURRENT_DESKTOP" = "GNOME" ]; then
-  if apt-cache show fragments > /dev/null; then # Install for debian-based distros
+if [ "$XDG_CURRENT_DESKTOP" = "GNOME" ]; then # Install fragments - Torrent client made for GNOME
+  if command -v apt-get > /dev/null && apt-cache search 'fragments' > /dev/null; then # Install for debian-based distros
     sudo apt-get install -y fragments
   else
     ./install-snap.sh
     sudo snap install fragments
   fi
-else
+else # Install qbittorrent - Just another torrent client
   if command -v apt-get > /dev/null; then # Install for debian-based distros
     sudo apt-get install -y qbittorrent
   fi
