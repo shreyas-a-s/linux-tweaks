2c651e0dc0a9523a35fd604494c5a9a02f5412ad
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-12-19 18:00:38 +0530

    Update documentation and change indentation

--- scripts/install-brave.sh
+++ scripts/install-brave.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
 if command -v apt-get > /dev/null; then # Install for debian-based distros
-    # Add repository
-    sudo apt-get update
-    sudo apt-get -y install curl
-    sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
-    echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-    
-    # To set third-party repositories to have least priority
-    echo 'Package: *\
-    \nPin: origin brave-browser-apt-release.s3.brave.com\
-    \nPin-Priority: 100' | sudo tee /etc/apt/preferences.d/brave.pref > /dev/null
-    
-    # Install the app
-    sudo apt-get update
-    sudo apt-get -y install brave-browser
+  # Add repository
+  sudo apt-get update
+  sudo apt-get -y install curl
+  sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+  echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+  
+  # To set third-party repositories to have least priority
+  echo 'Package: *\
+  \nPin: origin brave-browser-apt-release.s3.brave.com\
+  \nPin-Priority: 100' | sudo tee /etc/apt/preferences.d/brave.pref > /dev/null
+  
+  # Install the app
+  sudo apt-get update
+  sudo apt-get -y install brave-browser
 fi
 
