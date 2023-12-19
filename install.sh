ed41f1ca98e3f820dffbfa5697012585fc18627f
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-12-19 17:59:05 +0530

    Update documentation and change indentation

--- install.sh
+++ install.sh
@@ -17,27 +17,25 @@ fi
 SCRIPT_DIR=$(dirname -- "$( readlink -f -- "$0"; )") && cd "$SCRIPT_DIR" || exit
 
 # My custom scripts
-./scripts/update-system.sh # Updating installed programs
-./scripts/install-brave.sh # brave-browser
-./scripts/install-essential-apps.sh # As the name suggests
-./scripts/install-github-desktop.sh # github-desktop for linux
-./scripts/install-kde-or-gsconnect.sh # connect phone to computer for copy pasting files and command and more
-./scripts/install-nala.sh # apt, but colorful
-./scripts/install-onlyoffice.sh # office suite
-./scripts/install-passwd-manager.sh # As name suggests
-./scripts/install-torrent-client.sh # Also, as name suggests
-./scripts/install-ufw.sh # Install and Setup UFW - The Uncomplicated Firewall
-./scripts/install-vscodium.sh # open source vscode
-./scripts/setup-antix.sh # Antix Linux Customisations
-./scripts/setup-bootloader.sh # Bootloader Customisations
-./scripts/setup-gnome.sh # GNOME Desktop Environment Customisations
-./scripts/shell-customization.sh # bash/fish/zsh customizations
+./scripts/update-system.sh              # Updating installed programs
+./scripts/install-brave.sh              # My fav browser
+./scripts/install-essential-apps.sh     # As the name suggests
+./scripts/install-github-desktop.sh     # Github-desktop for linux
+./scripts/install-kde-or-gsconnect.sh   # connect phone to computer for copy pasting files and command and more
+./scripts/install-nala.sh               # Good old apt, but colorful
+./scripts/install-onlyoffice.sh         # Office suite
+./scripts/install-passwd-manager.sh     # As name suggests
+./scripts/install-torrent-client.sh     # Also, as name suggests
+./scripts/install-ufw.sh                # Install and Setup UFW - The Uncomplicated Firewall
+./scripts/install-vscodium.sh           # Open source vscode
+./scripts/setup-antix.sh                # Antix Linux Customisations
+./scripts/setup-bootloader.sh           # Bootloader Customisations
+./scripts/setup-gnome.sh                # GNOME Desktop Environment Customisations
+./scripts/shell-customization.sh        # Shell (bash/fish/zsh) customizations
 
-# Lower swappiness value for better utilization of RAM
-sudo sysctl vm.swappiness=10
-
-# Add script to toggle wifi
-sudo cp scripts/wifi-toggle.sh /usr/local/bin/wifi-toggle
+# Custom tweaks
+sudo sysctl vm.swappiness=10                               # Lower swappiness value for better utilization of RAM
+sudo cp scripts/wifi-toggle.sh /usr/local/bin/wifi-toggle  # Script to toggle wifi
 
 # Done
 echo "Installation is complete. Reboot your system for the changes to take place."
--- scripts/install-command-not-found.sh
+++ scripts/install-command-not-found.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-if command -v apt-get > /dev/null; then
+if command -v apt-get > /dev/null; then # Install for debian-based distros
   sudo apt-get install -y command-not-found
+
   # Update database of command-not-found
   sudo update-command-not-found
   sudo apt-get update
