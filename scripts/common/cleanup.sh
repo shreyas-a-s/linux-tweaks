c2d89deccea8245407504aebe7ba023d3d5453fc
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-12-20 21:02:30 +0530

    Make cleanup.sh archlinux-compatible

--- scripts/common/cleanup.sh
+++ scripts/common/cleanup.sh
@@ -4,6 +4,13 @@ if command -v apt-get > /dev/null; then # For debian-based distros
   sudo apt-get autoremove -y
 fi
 
+if command -v pacman > /dev/null; then # For archlinux-based distros
+  sudo pacman -Scc --noconfirm # Empty pacman cache
+  if command -v yay > /dev/null; then
+    yay -Scc --noconfirm # Empty aur package cache
+  fi
+fi
+
 # Add a cron-job to auto clear trash
 echo "@reboot $USER /usr/bin/find /home/$USER/.local/share/Trash/ -mtime +7 -delete" | sudo tee /etc/cron.d/auto-trash-empty > /dev/null
 
