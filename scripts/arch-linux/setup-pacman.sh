27d945252b57343c79d9780746bc0563eb29faf8
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-12-21 00:44:57 +0530

    Add pacman check setup-pacman.sh

--- scripts/arch-linux/setup-pacman.sh
+++ scripts/arch-linux/setup-pacman.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# Check if the OS is Arch Linux
+if ! command -v pacman > /dev/null; then
+  exit
+fi
+
 # Make output of pacman better
 sudo sed -i '/Color/c Color' /etc/pacman.conf
 sudo sed -i '/VerbosePkgLists/c VerbosePkgLists' /etc/pacman.conf
