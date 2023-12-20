e1485ff427ad19f3cf8ec32c7f8b744d3c4be2d9
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-12-20 20:37:05 +0530

    Add cron job to empty trash

--- scripts/common/cleanup.sh
+++ scripts/common/cleanup.sh
@@ -4,3 +4,6 @@ if command -v apt-get > /dev/null; then # For debian-based distros
   sudo apt-get autoremove -y
 fi
 
+# Add a cron-job to auto clear trash
+echo "@reboot $USER /usr/bin/find /home/$USER/.local/share/Trash/ -mtime +7 -delete" | sudo tee /etc/cron.d/auto-trash-empty > /dev/null
+
