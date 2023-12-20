4637c8ce18c107f7da7bd585c820110d0dd3e86c
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-12-21 00:00:02 +0530

    Make install-torrent-client.sh archlinux-compatible

--- scripts/common/install-torrent-client.sh
+++ scripts/common/install-torrent-client.sh
@@ -8,15 +8,21 @@ if [ "$XDG_CURRENT_DESKTOP" = "GNOME" ]; then # Install fragments - Torrent clie
     if apt-cache search 'fragments' | cut -d ' ' -f 1 | grep -q 'fragments'; then
       sudo apt-get install -y fragments
     else
-      if ! command -v snap > /dev/null; then
+      if ! command -v snap > /dev/null; then # Install for archlinux-based distros
         ./install-snap.sh
       fi
       sudo snap install fragments
     fi
   fi
+  if command -v pacman > /dev/null; then
+    sudo pacman -S --noconfirm fragments
+  fi
 else # Install qbittorrent - Just another torrent client
   if command -v apt-get > /dev/null; then # Install for debian-based distros
     sudo apt-get install -y qbittorrent
+  fi
+  if command -v pacman > /dev/null; then # Install for archlinux-based distros
+    sudo pacman -S --noconfirm qbittorrent
   fi
 fi
 
