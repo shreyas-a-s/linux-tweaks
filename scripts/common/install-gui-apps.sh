c379b178d06c87f509f33a14d1bd8547e4d3368a
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-12-20 21:23:01 +0530

    Make install-gui-apps.sh archlinux-compatible

--- scripts/common/install-gui-apps.sh
+++ scripts/common/install-gui-apps.sh
@@ -7,3 +7,7 @@ if command -v apt-get > /dev/null; then # Install for debian-based distros
   xargs -a "gui-apps.txt" sudo apt-get install -y python-is-python3
 fi
 
+if command -v pacman > /dev/null; then # Install for debian-based distros
+  xargs -a "gui-apps.txt" sudo pacman -S --noconfirm
+fi
+
