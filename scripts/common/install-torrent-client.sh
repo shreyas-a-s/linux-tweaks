0b0e7d31e6faf199bb37e168de9b5437d2ae0b7c
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-12-20 18:53:23 +0530

    Fix fragments app installation on debian

--- scripts/common/install-torrent-client.sh
+++ scripts/common/install-torrent-client.sh
@@ -5,7 +5,7 @@ SCRIPT_DIR=$(dirname -- "$( readlink -f -- "$0"; )") && cd "$SCRIPT_DIR" || exit
 
 if [ "$XDG_CURRENT_DESKTOP" = "GNOME" ]; then # Install fragments - Torrent client made for GNOME
   if command -v apt-get > /dev/null; then # Install for debian-based distros
-    if apt-cache search 'fragments' > /dev/null; then
+    if apt-cache search 'fragments' | cut -d ' ' -f 1 | grep -q 'fragments'; then
       sudo apt-get install -y fragments
     else
       if ! command -v snap > /dev/null; then
