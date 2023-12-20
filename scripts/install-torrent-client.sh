d94d707d0ee68adcbbef337f18a1eec8653fe3ce
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-12-20 14:03:08 +0530

    Update fragments install on debian-based dsitros

--- scripts/install-torrent-client.sh
+++ scripts/install-torrent-client.sh
@@ -4,13 +4,15 @@
 SCRIPT_DIR=$(dirname -- "$( readlink -f -- "$0"; )") && cd "$SCRIPT_DIR" || exit
 
 if [ "$XDG_CURRENT_DESKTOP" = "GNOME" ]; then # Install fragments - Torrent client made for GNOME
-  if command -v apt-get > /dev/null && apt-cache search 'fragments' > /dev/null; then # Install for debian-based distros
-    sudo apt-get install -y fragments
-  else
-    if ! command -v snap > /dev/null; then
-      ./install-snap.sh
+  if command -v apt-get > /dev/null; then # Install for debian-based distros
+    if apt-cache search 'fragments' > /dev/null; then
+      sudo apt-get install -y fragments
+    else
+      if ! command -v snap > /dev/null; then
+        ./install-snap.sh
+      fi
+      sudo snap install fragments
     fi
-    sudo snap install fragments
   fi
 else # Install qbittorrent - Just another torrent client
   if command -v apt-get > /dev/null; then # Install for debian-based distros
