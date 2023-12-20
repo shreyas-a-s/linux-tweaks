6e7fd7a10f94ae246ef233a77fccf6d05ec11bb0
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-12-20 21:26:27 +0530

    Make install-kde-or-gsconnect.sh archlinux-compatible

--- scripts/common/install-kde-or-gsconnect.sh
+++ scripts/common/install-kde-or-gsconnect.sh
@@ -3,8 +3,10 @@
 if [ "$XDG_CURRENT_DESKTOP" = "GNOME" ]; then
   busctl --user call org.gnome.Shell.Extensions /org/gnome/Shell/Extensions org.gnome.Shell.Extensions InstallRemoteExtension s gsconnect@andyholmes.github.io
 else
-  if command -v apt-get > /dev/null; then
+  if command -v apt-get > /dev/null; then # Install for debian-based distros
     sudo apt-get install -y kdeconnect
+  elif command -v pacman > /dev/null; then # Install for archlinux-based distros
+    sudo pacman -S --noconfirm kdeconnect
   fi
 fi
 
