1a64785af61e4e33d0dbc115d75b61fb8ebb4789
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-12-21 00:37:34 +0530

    Make update-system.sh archlinux-compatible

--- scripts/common/update-system.sh
+++ scripts/common/update-system.sh
@@ -4,3 +4,11 @@ if command -v apt-get > /dev/null; then # Update debian-based distros
   sudo apt-get update && sudo apt-get -y upgrade
 fi
 
+if command -v pacman > /dev/null; then # Update archlinux-based distros
+  sudo pacman -Syu --noconfirm
+fi
+
+if command -v yay > /dev/null; then # Update aur packages in archlinux if present
+  yay -Syu --noconfirm
+fi
+
