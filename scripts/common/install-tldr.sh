fb7a9dab812d580182d7366cd5bbf6318215d02d
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-12-20 23:56:28 +0530

    Make install-tldr.sh archlinux-compatible

--- scripts/common/install-tldr.sh
+++ scripts/common/install-tldr.sh
@@ -4,5 +4,9 @@ if command -v apt-get > /dev/null; then # Install for debian-based distros
   sudo apt-get install -y tldr
 fi
 
+if command -v pacman > /dev/null; then # Install for archlinux-based distros
+  sudo pacman -S --noconfirm tldr
+fi
+
 tldr -u # Update tldr database
 
