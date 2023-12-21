831aea29f4186f3329acd305c68fd49404623431
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-12-21 18:34:11 +0530

    Fix - E: No packages found error when using apt-cache to check if package is available

--- scripts/debian/install-nala.sh
+++ scripts/debian/install-nala.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if command -v apt-get > /dev/null && apt-cache show nala > /dev/null; then
+if command -v apt-get > /dev/null && apt-cache search nala | cut -d ' ' -f 1 | grep -q 'nala'; then
   sudo apt-get -y install nala
 fi
 
