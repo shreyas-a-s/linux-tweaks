e7b2d8eab515cadb5efa276fbefed21a35f223b8
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-12-21 21:13:47 +0530

    Separate checking if OS is debian-based & if nala is present in repo

--- scripts/debian/install-nala.sh
+++ scripts/debian/install-nala.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 
-if command -v apt-get > /dev/null && apt-cache search nala | cut -d ' ' -f 1 | grep -xq 'nala'; then
+# Check if the OS is Debian-based
+if ! command -v apt-get > /dev/null; then
+  exit
+fi
+
+# Install the program
+if apt-cache search nala | cut -d ' ' -f 1 | grep -xq 'nala'; then
   sudo apt-get -y install nala
 fi
 
