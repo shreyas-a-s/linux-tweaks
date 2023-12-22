b4a4b622ad9dd34b1ba1a4952eeaff2963b402a6
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-12-22 17:05:23 +0530

    Use array for storing xdg_programs since we are using bash as shebang

--- scripts/common/setup-xdg-base-dirs.sh
+++ scripts/common/setup-xdg-base-dirs.sh
@@ -8,20 +8,20 @@ fi
 # Change directory
 SCRIPT_DIR=$(dirname -- "$( readlink -f -- "$0"; )") && cd "$SCRIPT_DIR" || exit
 
-# Define a string of programs
-xdg_programs="xdg-user-dirs xdg-desktop-portal"
+# Define an array of programs
+xdg_programs=('xdg-user-dirs' 'xdg-desktop-portal')
 
 # Add xdg-desktop-portal-gnome if gnome is detected
 if ps -A | grep -q "gnome-shell"; then
-  xdg_programs="$xdg_programs xdg-desktop-portal-gnome"
+  xdg_programs+=('xdg-desktop-portal-gnome')
 fi
 
 # Install xdg apps
 if command -v apt-get > /dev/null; then # Install for debian-based distros
-  sudo apt-get install -y $xdg_programs
+  sudo apt-get install -y "${xdg_programs[@]}"
 fi
 if command -v pacman > /dev/null; then # Install for archlinux-based distros
-  sudo pacman -S --noconfirm $xdg_programs
+  sudo pacman -S --noconfirm "${xdg_programs[@]}"
 fi
 
 # Function to setup XDG user dirs
