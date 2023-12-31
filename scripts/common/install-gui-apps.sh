e87056237285a64246fafe57d3b0aba7d2214e74
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-12-31 16:47:15 +0530

    Move python-is-python3 from install-gui-apps.sh -> install-terminal-apps.sh

--- scripts/common/install-gui-apps.sh
+++ scripts/common/install-gui-apps.sh
@@ -9,7 +9,7 @@ fi
 SCRIPT_DIR=$(dirname -- "$( readlink -f -- "$0"; )") && cd "$SCRIPT_DIR" || exit
 
 if command -v apt-get > /dev/null; then # Install for debian-based distros
-  xargs -a "gui-apps.txt" sudo apt-get install -y python-is-python3
+  xargs -a "gui-apps.txt" sudo apt-get install -y
 fi
 
 if command -v pacman > /dev/null; then # Install for debian-based distros
--- scripts/common/install-terminal-apps.sh
+++ scripts/common/install-terminal-apps.sh
@@ -9,7 +9,7 @@ fi
 SCRIPT_DIR=$(dirname -- "$( readlink -f -- "$0"; )") && cd "$SCRIPT_DIR" || exit
 
 if command -v apt-get > /dev/null; then # Install for debian-based distros
-  xargs -a "terminal-apps.txt" sudo apt-get install -y gh fonts-noto-color-emoji
+  xargs -a "terminal-apps.txt" sudo apt-get install -y gh fonts-noto-color-emoji python-is-python3
 fi
 
 if command -v pacman > /dev/null; then # Install for archlinux-based distros
