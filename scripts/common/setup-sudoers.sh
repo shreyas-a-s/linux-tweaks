55191f8b94134711d74527632e0cf0a23becbefb
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-01-03 12:40:41 +0530

    Fix - comparison of sudo version

--- scripts/common/setup-sudoers.sh
+++ scripts/common/setup-sudoers.sh
@@ -6,8 +6,13 @@ echo 'Defaults    pwfeedback' | sudo tee -a /etc/sudoers > /dev/null
 # Store version number of sudo into a variable
 sudo_version=$(command sudo -V | grep "Sudo version" | awk '{print $NF}' | awk -F 'p' '{print $1}')
 
+# Split the version numbers into arrays
+IFS='.' read -ra sudo_version_array <<< "$sudo_version"
+compare_version_array=(1 9 6)
+
 # Compare version of sudo with 1.9.6 (which is required for the next sudoers tweak to work)
-compare_versions=$(awk 'BEGIN{print ("'$sudo_version'" >= "'1.9.6'")}')
+compare_versions=$(awk -v sv1="${sudo_version_array[0]}" -v sv2="${sudo_version_array[1]}" -v sv3="${sudo_version_array[2]}" \
+  'BEGIN{printf("%d\n", (sv1 > 1 || (sv1 == 1 && (sv2 > 9 || (sv2 == 9 && sv3 >= 6)))))}')
 
 # Disable creation of ~/.sudo_as_admin_successful
 if [ "$compare_versions" -eq 1 ]; then # Apply tweak only if sudo version is greater than or equal to 1.9.6
