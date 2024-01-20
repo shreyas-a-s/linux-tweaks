1ac1b06bb0d005569a4d9da973f6fd73aa9ba0ca
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-01-20 21:24:22 +0530

    Install apt-file along with command-not-found

--- scripts/common/install-command-not-found.sh
+++ scripts/common/install-command-not-found.sh
@@ -6,8 +6,8 @@ if type _printtitle &> /dev/null; then
 fi
 
 if command -v apt-get > /dev/null; then # Install for debian-based distros
-  # Install the app
-  sudo apt-get install -y command-not-found
+  # Install the app & dependencies
+  sudo apt-get install -y command-not-found apt-file
 
   # Update database of command-not-found
   sudo update-command-not-found
