ef5c62a442316debfe1e1f90575135a7cb27b21b
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-01-24 20:01:53 +0530

    Add indic fonts

--- scripts/common/install-terminal-apps.sh
+++ scripts/common/install-terminal-apps.sh
@@ -9,10 +9,10 @@ fi
 SCRIPT_DIR=$(dirname -- "$( readlink -f -- "$0"; )") && cd "$SCRIPT_DIR" || exit
 
 if command -v apt-get > /dev/null; then # Install for debian-based distros
-  xargs -a "terminal-apps.txt" sudo apt-get install -y gh fonts-noto-color-emoji python-is-python3
+  xargs -a "terminal-apps.txt" sudo apt-get install -y gh fonts-indic fonts-noto-color-emoji python-is-python3
 fi
 
 if command -v pacman > /dev/null; then # Install for archlinux-based distros
-  xargs -a "terminal-apps.txt" sudo pacman -S --noconfirm github-cli noto-fonts noto-fonts-cjk noto-fonts-emoji
+  xargs -a "terminal-apps.txt" sudo pacman -S --noconfirm github-cli noto-fonts noto-fonts-cjk noto-fonts-emoji ttf-indic-otf
 fi
 
