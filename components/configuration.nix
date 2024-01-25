a2dc7161dc9d156202d7d7276083468090f9b6cb
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-01-25 18:28:15 +0530

    Add kdeconnect firewall rules to configuration.nix

--- components/configuration.nix
+++ components/configuration.nix
@@ -184,6 +184,15 @@
   # services.openssh.enable = true;
 
   # Open ports in the firewall.
+  networking.firewall = {
+    enable = true;
+    allowedTCPPortRanges = [
+      { from = 1714; to = 1764; } # KDE Connect / GSConnect
+    ];
+    allowedUDPPortRanges = [
+      { from = 1714; to = 1764; } # KDE Connect / GSConnect
+    ];
+  };
   # networking.firewall.allowedTCPPorts = [ ... ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
