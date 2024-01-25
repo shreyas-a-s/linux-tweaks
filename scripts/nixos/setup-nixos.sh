46e8ace57d3f7b46f8c7fc1617bacb64a6de8c2b
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-01-26 01:26:51 +0530

    Create setup-nixos.sh

---
+++ scripts/nixos/setup-nixos.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Display title of script
+if type _printtitle &> /dev/null; then
+  _printtitle "SETTING UP - NIXOS"
+fi
+
+# Change directory
+SCRIPT_DIR=$(dirname -- "$( readlink -f -- "$0"; )") && cd "$SCRIPT_DIR" || exit
+
+# Backup current nixos config file
+sudo mv /etc/nixos/configuration.nix /etc/nixos/configuration.nix.bak
+
+# Copy our new nixos config file
+sudo cp ../../components/configuration.nix /etc/nixos/configuration.nix
+
+# Rebuild the system configuration
+sudo nixos-rebuild switch --log-format bar-with-logs
+
+# Reclaiming disk space using Nix garbage collector
+sudo nix-collect-garbage
+
