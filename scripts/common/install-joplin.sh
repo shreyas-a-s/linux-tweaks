1e007819e2fdbf66be6ab0eb022ccd6a24475d83
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-01-27 00:32:10 +0530

    Add RHEL-support to install-joplin.sh

--- scripts/common/install-joplin.sh
+++ scripts/common/install-joplin.sh
@@ -8,8 +8,8 @@ fi
 # Change directory
 SCRIPT_DIR=$(dirname -- "$( readlink -f -- "$0"; )") && cd "$SCRIPT_DIR" || exit
 
-# Install neovim
-if command -v apt-get > /dev/null; then # Install for debian-based distros
+# Install for debian-based distros
+if command -v apt-get > /dev/null; then
   if apt-cache search 'joplin' | cut -d ' ' -f 1 | grep -xq 'joplin'; then
     sudo apt-get install -y joplin
   else
@@ -20,11 +20,20 @@ if command -v apt-get > /dev/null; then # Install for debian-based distros
   fi
 fi
 
-if command -v pacman > /dev/null; then # Install for archlinux-based distros
+# Install for archlinux-based distros
+if command -v pacman > /dev/null; then
   if pacman -Ss joplin-desktop > /dev/null; then
     sudo pacman -S --noconfirm joplin-desktop
   elif command -v yay > /dev/null; then
     yay -S --noconfirm joplin-appimage
   fi
+fi
+
+# Install for RHEL-based distros
+if command -v dnf > /dev/null; then
+  if ! command -v snap > /dev/null; then
+    ./install-snap.sh
+  fi
+  sudo snap install joplin-desktop
 fi
 
