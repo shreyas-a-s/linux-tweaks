5dfed70d06dbca148dd34f305c28b38a7f2c3be7
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-01-27 01:22:12 +0530

    Add RHEL-support to setup-bluetooth.sh

--- scripts/common/setup-bluetooth.sh
+++ scripts/common/setup-bluetooth.sh
@@ -10,6 +10,8 @@ if command -v apt-get > /dev/null; then
   sudo apt install -y blueman bluez
 elif command -v pacman > /dev/null; then
   sudo pacman -S --noconfirm blueman bluez bluez-hid2hci
+elif command -v dnf > /dev/null; then
+  sudo dnf install -y blueman bluez bluez-hid2hci
 fi
 
 # Make connecting to bluetooth sound devices better
