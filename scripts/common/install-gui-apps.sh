c96f9ff7f20a98fe924a488edd5918b9dc54be72
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-01-27 00:27:50 +0530

    Add RHEL-support to install-gui-apps.sh

--- scripts/common/install-gui-apps.sh
+++ scripts/common/install-gui-apps.sh
@@ -8,11 +8,18 @@ fi
 # Change directory
 SCRIPT_DIR=$(dirname -- "$( readlink -f -- "$0"; )") && cd "$SCRIPT_DIR" || exit
 
-if command -v apt-get > /dev/null; then # Install for debian-based distros
+# Install for debian-based distros
+if command -v apt-get > /dev/null; then
   xargs -a "../../components/gui-apps.txt" sudo apt-get install -y
 fi
 
-if command -v pacman > /dev/null; then # Install for archlinux-based distros
+# Install for archlinux-based distros
+if command -v pacman > /dev/null; then
   xargs -a "../../components/gui-apps.txt" sudo pacman -S --noconfirm qt6-wayland
+fi
+
+# Install for RHEL-based distros
+if command -v dnf > /dev/null; then
+  xargs -a "../../components/gui-apps.txt" sudo dnf install -y
 fi
 
