7041b4767748679e42d444f7e3e931d79aae2056
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-01-27 00:24:03 +0530

    Add RHEL-support to install-github-desktop.sh

--- scripts/common/install-github-desktop.sh
+++ scripts/common/install-github-desktop.sh
@@ -29,3 +29,10 @@ if command -v pacman > /dev/null; then # Install for archlinux-based distros
   fi
 fi
 
+# Install for RHEL-based distros
+if command -v dnf > /dev/null; then
+  sudo rpm --import https://rpm.packages.shiftkey.dev/gpg.key
+  sudo sh -c 'echo -e "[shiftkey-packages]\nname=GitHub Desktop\nbaseurl=https://rpm.packages.shiftkey.dev/rpm/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://rpm.packages.shiftkey.dev/gpg.key" > /etc/yum.repos.d/shiftkey-packages.repo'
+  sudo dnf install -y github-desktop
+fi
+
