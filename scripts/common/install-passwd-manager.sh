587b588905fa549ec50fb64e471f222e0271a6a8
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-01-27 01:01:46 +0530

    Add RHEL-support to install-passwd-manager.sh

--- scripts/common/install-passwd-manager.sh
+++ scripts/common/install-passwd-manager.sh
@@ -10,11 +10,15 @@ if ps -A | grep -q "gnome-shell"; then # Install secrets (password manager for G
     sudo apt-get install -y secrets
   elif command -v pacman > /dev/null; then
     sudo pacman -S --noconfirm secrets
+  elif command -v dnf > /dev/null; then
+    sudo dnf install -y secrets
   fi
 else # Install Keepassxc (another password manager)
   if command -v apt-get > /dev/null; then
     sudo apt-get install -y keepassxc
   elif command -v pacman > /dev/null; then
     sudo pacman -S --noconfirm keepassxc
+  elif command -v dnf > /dev/null; then
+    sudo dnf install -y keepassxc
   fi
 fi
