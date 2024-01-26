0dfe4055d1392e1c34922356f7c4c7a8f263a58e
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-01-27 01:12:19 +0530

    Add RHEL-support to install-terminal-apps.sh

--- scripts/common/install-terminal-apps.sh
+++ scripts/common/install-terminal-apps.sh
@@ -8,11 +8,18 @@ fi
 # Change directory
 SCRIPT_DIR=$(dirname -- "$( readlink -f -- "$0"; )") && cd "$SCRIPT_DIR" || exit
 
-if command -v apt-get > /dev/null; then # Install for debian-based distros
+# Install for debian-based distros
+if command -v apt-get > /dev/null; then
   xargs -a "../../components/terminal-apps.txt" sudo apt-get install -y gh fonts-indic fonts-noto-color-emoji python-is-python3
 fi
 
-if command -v pacman > /dev/null; then # Install for archlinux-based distros
+# Install for archlinux-based distros
+if command -v pacman > /dev/null; then
   xargs -a "../../components/terminal-apps.txt" sudo pacman -S --noconfirm github-cli noto-fonts noto-fonts-cjk noto-fonts-emoji ttf-indic-otf
+fi
+
+# Install for RHEL-based distros
+if command -v dnf > /dev/null; then
+  xargs -a "../../components/terminal-apps.txt" sudo dnf install -y gh google-noto-cjk-fonts google-noto-emoji-fonts
 fi
 
