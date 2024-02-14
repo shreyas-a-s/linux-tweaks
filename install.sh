56c1a0069ce8e7f48672c464a81f1bf8a9616ddf
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-02-14 21:35:19 +0530

    Fix - choices not getting used correctly

--- install.sh
+++ install.sh
@@ -68,29 +68,32 @@ if [ $exitstatus = 1 ]; then
   exit 1
 fi
 
+# Convert choices variable into an array
+eval "choices=($choices)"
+
 # My custom scripts
 ./scripts/archlinux/install-aur-helper.sh      # Program that helps install packages from AUR (the user contributed arch linux repository)
 ./scripts/archlinux/install-paccache.sh        # Program that helps clear pacman cache in archlinux
 ./scripts/archlinux/setup-pacman.sh            # Make arch package manager work better
 ./scripts/common/update-system.sh              # Updating installed programs
-_is_element_of_array c_array[@] 1  && ./scripts/common/install-auto-cpufreq.sh       # Automatically change cpu freq to save battery
-_is_element_of_array c_array[@] 2  && ./scripts/common/install-brave.sh              # My fav browser
-_is_element_of_array c_array[@] 3  && ./scripts/common/install-cron.sh               # Program to automate tasks
-_is_element_of_array c_array[@] 4  && ./scripts/common/install-fonts.sh              # Install some fonts (eg: region-specific ones)
-_is_element_of_array c_array[@] 5  && ./scripts/common/install-github-desktop.sh     # Github-desktop for linux
-_is_element_of_array c_array[@] 6  && ./scripts/common/install-gui-apps.sh           # GUI apps common to all default package managers
-_is_element_of_array c_array[@] 7  && ./scripts/common/install-joplin.sh             # Cloud-synced note-taking app
-_is_element_of_array c_array[@] 8  && ./scripts/common/install-kde-or-gsconnect.sh   # connect phone to computer for copy pasting files & text
-_is_element_of_array c_array[@] 9  && ./scripts/common/install-onlyoffice.sh         # Office suite
-_is_element_of_array c_array[@] 10 && ./scripts/common/install-passwd-manager.sh     # As name suggests
-_is_element_of_array c_array[@] 11 && ./scripts/common/install-terminal-tweaks.sh    # Some customizations to make my terminal experience better
-_is_element_of_array c_array[@] 12 && ./scripts/common/install-torrent-client.sh     # Also, as name suggests
-_is_element_of_array c_array[@] 13 && ./scripts/common/install-ufw.sh                # Install and Setup UFW - The Uncomplicated Firewall
-_is_element_of_array c_array[@] 14 && ./scripts/common/install-vscodium.sh           # Open source vscode
-_is_element_of_array c_array[@] 15 && ./scripts/common/setup-bluetooth.sh            # Bluetooth tweaks
-_is_element_of_array c_array[@] 16 && ./scripts/common/setup-bootloader.sh           # Bootloader Customisations
-_is_element_of_array c_array[@] 17 && ./scripts/common/setup-gnome.sh                # GNOME Desktop Environment Customisations
-_is_element_of_array c_array[@] 18 && ./scripts/common/setup-multi-audio.sh          # Enable audio routed to multiple devices at same time
+_is_element_of_array choices[@] 1  && ./scripts/common/install-auto-cpufreq.sh       # Automatically change cpu freq to save battery
+_is_element_of_array choices[@] 2  && ./scripts/common/install-brave.sh              # My fav browser
+_is_element_of_array choices[@] 3  && ./scripts/common/install-cron.sh               # Program to automate tasks
+_is_element_of_array choices[@] 4  && ./scripts/common/install-fonts.sh              # Install some fonts (eg: region-specific ones)
+_is_element_of_array choices[@] 5  && ./scripts/common/install-github-desktop.sh     # Github-desktop for linux
+_is_element_of_array choices[@] 6  && ./scripts/common/install-gui-apps.sh           # GUI apps common to all default package managers
+_is_element_of_array choices[@] 7  && ./scripts/common/install-joplin.sh             # Cloud-synced note-taking app
+_is_element_of_array choices[@] 8  && ./scripts/common/install-kde-or-gsconnect.sh   # connect phone to computer for copy pasting files & text
+_is_element_of_array choices[@] 9  && ./scripts/common/install-onlyoffice.sh         # Office suite
+_is_element_of_array choices[@] 10 && ./scripts/common/install-passwd-manager.sh     # As name suggests
+_is_element_of_array choices[@] 11 && ./scripts/common/install-terminal-tweaks.sh    # Some customizations to make my terminal experience better
+_is_element_of_array choices[@] 12 && ./scripts/common/install-torrent-client.sh     # Also, as name suggests
+_is_element_of_array choices[@] 13 && ./scripts/common/install-ufw.sh                # Install and Setup UFW - The Uncomplicated Firewall
+_is_element_of_array choices[@] 14 && ./scripts/common/install-vscodium.sh           # Open source vscode
+_is_element_of_array choices[@] 15 && ./scripts/common/setup-bluetooth.sh            # Bluetooth tweaks
+_is_element_of_array choices[@] 16 && ./scripts/common/setup-bootloader.sh           # Bootloader Customisations
+_is_element_of_array choices[@] 17 && ./scripts/common/setup-gnome.sh                # GNOME Desktop Environment Customisations
+_is_element_of_array choices[@] 18 && ./scripts/common/setup-multi-audio.sh          # Enable audio routed to multiple devices at same time
 ./scripts/common/cleanup.sh                    # Cleanup package manager cache
 ./scripts/debian/install-nala.sh               # Good old apt, but colorful
 ./scripts/debian/setup-antix.sh                # Antix Linux Customisations
