0fa85f678e0facfb6bebeec6c3412dca1dd5d44a
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-02-26 19:15:22 +0530

    Remove unnecessary 'change directory' line from install-fonts.sh

--- scripts/common/install-fonts.sh
+++ scripts/common/install-fonts.sh
@@ -5,9 +5,6 @@ if type _printtitle &> /dev/null; then
   _printtitle "INSTALLING - FONTS"
 fi
 
-# Change directory
-SCRIPT_DIR=$(dirname -- "$( readlink -f -- "$0"; )") && cd "$SCRIPT_DIR" || exit
-
 # Install for debian-based distros
 if command -v apt-get > /dev/null; then
   sudo apt-get install -y fonts-indic fonts-noto-color-emoji
