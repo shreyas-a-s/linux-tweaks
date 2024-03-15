c3f89b5b4e77cacf547c5c7ce21a0ab92ab86288
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-03-15 16:15:31 +0530

    fix: Enable automatic installation of Brave
    - Just added -y flag to apt install

--- scripts/install-brave.sh
+++ scripts/install-brave.sh
@@ -3,4 +3,4 @@ sudo apt install curl
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 sudo apt update
-sudo apt install brave-browser
+sudo apt install -y brave-browser
