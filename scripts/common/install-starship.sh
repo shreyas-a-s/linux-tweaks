3fe964a1aeaf30f12e58107293cd2a5ddc358945
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2024-03-19 12:05:52 +0530

    feat(install-starship.sh): Install starship using pacman for arch linux

--- scripts/common/install-starship.sh
+++ scripts/common/install-starship.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
-curl -sS https://starship.rs/install.sh | sh -s -- -y
+if command -v pacman > /dev/null; then # Install for archlinux-based distros
+  sudo pacman -S --noconfirm starship
+else # Install for other linux distros
+  curl -sS https://starship.rs/install.sh | sh -s -- -y
+fi
 
